9bec4d787bd98d76598ba342d0592a7a68ae745b
CarinWood <carin.wood.85@gmail.com>
2024-02-05 22:12:08 +0100

    update

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ fetch("https://majazocom.github.io/Data/pokemons.json")
     return response.json();
   })
   .then((data) => {
-
     data.forEach((element) => {
       let pokemon = document.createElement("p");
       pokemon.innerText = element.name + " ";
@@ -22,7 +21,7 @@ fetch("https://majazocom.github.io/Data/pokemons.json")
   });
 
 /* Exercise 2 */
-let dogs = document.querySelector('.dogs')
+let dogs = document.querySelector(".dogs");
 
 fetch("https://majazocom.github.io/Data/dogs.json")
   .then((res) => {
@@ -34,13 +33,57 @@ fetch("https://majazocom.github.io/Data/dogs.json")
   })
   .then((data) => {
     data.forEach((dog) => {
-      let newDog = document.createElement('p')
-      newDog.innerText = dog.name
-      dogs.appendChild(newDog)
+      let newDog = document.createElement("p");
+      newDog.innerText = dog.name;
+      dogs.appendChild(newDog);
     });
   });
 
-
 /*   Exercise 3 */
+let books = document.querySelector(".books");
 
+fetch("https://majazocom.github.io/Data/books.json")
+  .then((res) => {
+    if (!res.ok) {
+      throw new Error(`Network response was not ok: ${res.statusText}`);
+    }
 
+    return res.json();
+  })
+  .then((data) => {
+    data.forEach((book) => {
+      if (book.pages < 500) {
+        let newBook = document.createElement("p");
+        newBook.innerText = book.title;
+        books.appendChild(newBook);
+      }
+    });
+  });
+
+/* Exercise 4 */
+let persons = document.querySelector(".persons");
+let allergies = document.querySelector(".allergies");
+fetch("https://majazocom.github.io/Data/attendees.json")
+  .then((res) => {
+    if (!res.ok) {
+      throw new Error(`Network response was not ok: ${res.statusText}`);
+    }
+    return res.json();
+  })
+  .then((data) => {
+    data.forEach((person) => {
+      if (person.attending) {
+        console.log(person);
+        let newPerson = document.createElement("p");
+        newPerson.innerText = person.name;
+        persons.appendChild(newPerson);
+      }
+    });
+    data.forEach((person) => {
+      if (person.attending && person.allergies.length > 0) {
+        let allergy = document.createElement("p");
+        allergy.innerText = person.name;
+        allergies.appendChild(allergy);
+      }
+    });
+  });
